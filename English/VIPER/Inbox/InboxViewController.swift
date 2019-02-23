65f5ffc4b6888c3b7a4591757167d21823244f35
sng1996 <seregagavrilko@mail.ru>
2019-02-23 14:07:08 +0800

    Add InboxVIPER

--- English/VIPER/Inbox/InboxViewController.swift
+++ English/VIPER/Inbox/InboxViewController.swift
@@ -10,12 +10,26 @@ import UIKit
 
 class InboxViewController: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var presenter: InboxPresenterProtocol!
+    var configurator: InboxConfiguratorProtocol = InboxConfigurator()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    init() {
+        super.init(frame: .zero)
+        configurator.configure(with: self)
+        setupViews()
+        presenter.configureView()
+    }
+    
+    func setupViews() {
+        
+    }
+    
+}
 
+extension InboxViewController: InboxViewProtocol {
+    
 }
