bf871f37d9790ee3d2824836584e1babb8f4bcea
sng1996 <seregagavrilko@mail.ru>
2019-02-23 16:45:14 +0800

    Add SingleThemeVIPER

---
+++ English/VIPER/SingleTheme/SingleThemePresenter.swift
@@ -0,0 +1,25 @@
+//
+//  SingleThemePresenter.swift
+//  English
+//
+//  Created by Сергей Гаврилко on 23/02/2019.
+//  Copyright © 2019 gavrilko. All rights reserved.
+//
+
+import UIKit
+
+class SingleThemePresenter {
+    
+    weak var view: SingleThemeViewProtocol!
+    var interactor: SingleThemeInteractorProtocol!
+    var router: SingleThemeRouterProtocol!
+    
+    required init(view: SingleThemeViewProtocol) {
+        self.view = view
+    }
+    
+}
+
+extension SingleThemePresenter: SingleThemePresenterProtocol {
+    
+}
