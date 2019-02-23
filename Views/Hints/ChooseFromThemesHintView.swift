9c7b18f5d8285381df057adfd384798d9070c927
Сергей Гаврилко <sergeigavrilko@MacBook-Pro-Sergej-2.local>
2019-02-23 14:42:49 +0800

    Add ChooseFromThemesHintView

---
+++ Views/Hints/ChooseFromThemesHintView.swift
@@ -0,0 +1,15 @@
+//
+//  ChooseFromThemesHintView.swift
+//  English
+//
+//  Created by Сергей Гаврилко on 23/02/2019.
+//  Copyright © 2019 gavrilko. All rights reserved.
+//
+
+import UIKit
+
+class ChooseFromThemesHintView: UIView {
+
+    
+
+}
--- Views/Hints/RememberHint/RememberHintView.swift
+++ Views/Hints/RememberHint/RememberHintView.swift
@@ -14,6 +14,8 @@ protocol RememberHintViewDelegate {
 }
 
 class RememberHintView: UIView {
+    
+    let container = UIView()
 
     let headerLabel = UILabel(
         text: "Хотите, чтобы мы\nнапоминали вам\nо повторении слов?",
@@ -30,7 +32,8 @@ class RememberHintView: UIView {
     let footerLabel = UILabel(
         text: "Вы сможете настроить время уведомлений,\nлибо отключить их в разделе настройки.",
         color: UIColor(rgb: 0xCECECE),
-        font: UIFont.book(16)
+        font: UIFont.book(16),
+        alignment: .center
     )
     
     var delegate: RememberHintViewDelegate!
@@ -49,11 +52,26 @@ class RememberHintView: UIView {
         acceptButton.tapHandler = delegate.didTapAcceptButton
         declineButton.tapHandler = delegate.didTapDeclineButton
         
-        addSubview(headerLabel)
-        addSubview(notificationView)
-        addSubview(acceptButton)
-        addSubview(declineButton)
+        addSubview(container)
         addSubview(footerLabel)
+        container.addSubview(headerLabel)
+        container.addSubview(notificationView)
+        container.addSubview(acceptButton)
+        container.addSubview(declineButton)
+        
+        addConstraintsWithFormat(format: "H:|-\(Screen.sideInset)-[v0]-\(Screen.sideInset)-|", views: container)
+        addConstraintsWithFormat(format: "H:|-\(Screen.sideInset)-[v0]-\(Screen.sideInset)-|", views: footerLabel)
+        addConstraintsWithFormat(format: "V:[v0]-\(44 * Screen.heightCoef)-|", views: footerLabel)
+        
+        addConstraintsWithFormat(format: "H:|[v0]|", views: headerLabel)
+        addConstraintsWithFormat(format: "H:|[v0]|", views: notificationView)
+        addConstraintsWithFormat(format: "V:|[v0]-\(40 * Screen.heightCoef)-[v1]-\(60 * Screen.heightCoef)-[v2]-5-[v3]|", views: headerLabel, notificationView, acceptButton, declineButton)
+        
+        NSLayoutConstraint.activate([
+            acceptButton.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+            declineButton.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+            container.centerYAnchor.constraint(equalTo: centerYAnchor),
+        ])
     }
 
 }
